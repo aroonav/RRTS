139a35a909f5e02348b756fbeb98fedccdbb6475
Aroonav Mishra <aroonav11@gmail.com>
2016-11-18 03:00:49 +0530

    Completed repair scheduler code for supervisor.

--- rrts.cpp
+++ rrts.cpp
@@ -175,34 +175,75 @@ int supervisor::enter_information(int requestID, int newPriority, string newRawM
 
 int supervisor::repair_scheduler()
 {
-	// string str;
+	int i=0, activeRequests=-1, j=0, k=0, swap;
+	fstream fin, fout;
+	fin.open(ACTIVEREQUESTSPATH, ios::in);
+	fin>>activeRequests;						// Retrieves last request ID from file.
+	fin.close();
 
-	// fstream fin, fout;
-	// fin.open(DATAPATH, ios::in);
-	// fout.open(TEMPDATAPATH, ios::out);
+	string str[activeRequests+1], swap_str;		// str stores each road repair request.
+	int priorities[activeRequests+1];			// priorities stores the priority of each road repair request.
 
-	// if(fout.is_open())
-	// {
-	// 	while(getline(fin, str))
-	// 	{
-	// 		for (int i = 0; i < 12; i++)
-	// 		{
-	// 			cin.getline(tempStr, 128, ',');
-	// 			temp[i] = tempStr;		// temp[] contains the 12 pieces of information that was stored in the CSV file
-	// 		}
-	// 	}
-	// 	// Rename temp.txt to data.txt now.
-	// 	rename(TEMPDATAPATH, DATAPATH);
-	// 	remove(TEMPDATAPATH);
-	// 	cout<<"Values updated successfully !!"<<endl;
-	// }
-	// else
-	// {	cout<<"Error opening file while updating information by supervisor."<<endl;
-	// 	return 1;
-	// }
-	// fin.close();
-	// fout.close();
-	// return 0;
+	fin.open(DATAPATH, ios::in);
+
+	if(fin.is_open())
+	{
+		while(getline(fin, str[i]))
+		{
+			istringstream in(str[i]);
+			char tempStr[128];
+			for (int i = 0; i < 9; i++)
+				in.getline(tempStr, 128, ',');
+			priorities[i] = stoi(tempStr);
+			if(priorities[i]==-1)
+				priorities[i]=1000;
+			i++;
+		}
+	}
+	else
+	{	cout<<"Error opening file while updating information by supervisor."<<endl;
+		return 1;
+	}
+
+	// TODO: Use a divide and conquer approach sorting algorithm.
+	// Bubble sort according to the road repair priority and alongside also sort the array str.
+	for(j=0; j<(activeRequests-1); j++)
+	{
+		for(k=0; k<(activeRequests-j-1); k++)
+		{
+			if(priorities[k] > priorities[k+1])
+			{
+				swap			= priorities[k];
+				priorities[k]   = priorities[k+1];
+				priorities[k+1] = swap;
+
+				swap_str = str[k];
+				str[k] = str[k+1];
+				str[k+1] = swap_str;
+			}
+		}
+	}
+	fin.close();
+
+	fout.open(DATAPATH, ios::out);
+	cout<<"Data format: <Request ID> <Location> <Start Point> <End Point> <Name> <Address> <Contact> <ID Numbe> <Priority> <Required Raw Materials> <Required Machines> <Required Personnels>";
+	for(j=0; j<activeRequests; j++)
+	{
+		// Write sorted repair requests to file
+		fout<<str[j]<<endl;
+
+		// Now print the sorted repair requests to console. This is to generate the "report".
+		istringstream in(str[j]);
+		char tempStr[128];
+		for (int i = 0; i < 12; i++)
+		{
+			in.getline(tempStr, 128, ',');
+			cout<<tempStr<<" ";
+		}
+		cout<<endl;
+	}
+	fout.close();
+	return 0;
 }
 
 class mayor
@@ -292,7 +333,7 @@ int supervisor_driver()
 	}
 	else
 	{
-		cout<<"Now scheduling road repairs...";
+		cout<<"Now scheduling road repairs..."<<endl;
 		return s.repair_scheduler();
 	}
 }
